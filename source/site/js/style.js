3ab96134b1f2c35ca2402240e242c1b6bc4d21f6
joelvaneenwyk <joel.vaneenwyk@gmail.com>
2015-05-24 17:58:39 -0500

    Switched to using Bootstrap and removed a lot of garbage files.

--- source/site/js/style.js
+++ source/site/js/style.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {    
-	var clientHeight = ($(HTMLDocument).innerHeight() / 2) + 'px';
-	$(".top-half").css('height', clientHeight);
 });
 
-$(document).resize(function() {
-    alert('hi');
+$(window).on('resize', function(){
+	var clientHeight = ($(HTMLDocument).innerHeight() / 2) + 'px';
+	var clientWidth = $(HTMLDocument).innerWidth() + 'px';
+    $('body.div.div').height(clientHeight);
+	$("div#top.row").css('height', clientHeight);
+	$(".top").css('width', clientWidth);
+	$(".top-body").css('width', $(".top-body").width());
 });
