f5c53fe82863909e9859775cbe0cd3672137bcd6
unknown <jovaneen@MININT-7K0E1D4.northamerica.corp.microsoft.com>
2016-01-17 19:05:24 -0800

    Added new version of D3.js, created a playground for testing, and updated the nodejs server to work with the playground. Very very WIP and not clean at all, but getting this in for testing.

--- source/nodejs/server.js
+++ source/nodejs/server.js
@@ -1,17 +1,35 @@
 ﻿var connect = require('connect');
 var path = require('path');
 var serveStatic = require('serve-static');
-var http = require('http')
+var http = require('http');
+var finalhandler = require('finalhandler');
 
-var siteRoot = __dirname + '/../site';
+var siteRoot = __dirname + '/../../';
 var port = 8080;
-var serve = serveStatic(siteRoot, {});
-var app = connect();
 
 siteRoot = path.normalize(siteRoot);
 console.log('Starting server [http://localhost:' + port + '/');
 console.log('File server root: ' + siteRoot);
 
-app.use(serve);
+//var serve = serveStatic(siteRoot, {});
+//var app = connect();
+//app.use(serve);
 
-http.createServer(app).listen(port);
+
+//var server = http.createServer(function (req, res) {
+//    var done = finalhandler(req, res)
+//    serve(req, res, done)
+//})
+
+//var app = express()
+var express = require('express')
+var app = express()
+app.use(serveStatic(siteRoot + 'playground'))
+//app.use(serveStatic(siteRoot + 'source\test'))
+app.listen(port)
+
+//server.listen(port);
+
+//http.createServer(function (req, res) {
+//    file.serve(req, res);
+//}).listen(8080);
