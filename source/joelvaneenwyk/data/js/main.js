0cd5803cc70acda9dff3da8e100273e1012eb144
unknown <jovaneen@MININT-7K0E1D4.northamerica.corp.microsoft.com>
2016-04-05 18:16:33 -0700

    Prettify pass on source files.

--- source/joelvaneenwyk/data/js/main.js
+++ source/joelvaneenwyk/data/js/main.js
@@ -2,19 +2,16 @@
 // Event handlers
 //
 
-function log(info)
-{
+function log(info) {
     console.log("[JVE] " + info);
 }
 
-function onResize()
-{
+function onResize() {
     log("Resize event called...");
     updateHeader();
 }
 
-function onReady()
-{
+function onReady() {
     log("On ready called...");
     updateHeader();
 }
@@ -23,24 +20,23 @@ function onReady()
 // Global utilities
 //
 
-function updateHeader()
-{    
+function updateHeader() {
     var height = this.innerHeight / 2;
     var width = this.innerWidth;
-    
+
     //if (height < 250) height = 250;
     if (width < 100) width = 100;
-    
+
     // Force the width of the main container to be the width of the window
-	$(".container.parent").css('width', width + 'px');
+    $(".container.parent").css('width', width + 'px');
     // Set the top row to be half the page height
-	$("div.row.top").css('height', height + 'px');
-    
+    $("div.row.top").css('height', height + 'px');
+
     // Center the top header row by setting the width and the margin-left offset
     var middleWidth = this.innerWidth;
     if (middleWidth > 550) middleWidth = 550;
-	$(".row.header").css('width', middleWidth + 'px');
-	$(".row.header.centered").css('margin-left', '-' + (middleWidth / 2) + 'px');
+    $(".row.header").css('width', middleWidth + 'px');
+    $(".row.header.centered").css('margin-left', '-' + (middleWidth / 2) + 'px');
 }
 
 $(document).ready(function() {
