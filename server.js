6364f7e020f59adebae0a4f47276a4cc8bcca768
unknown <jovaneen@MININT-7K0E1D4.northamerica.corp.microsoft.com>
2016-04-07 15:16:00 -0700

    Added 404 and 500 errors back and cleaned them up. Added 'html' dependency.

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ var app = express()
 
 console.log('Root Server Started');
 
-
 app.use(
-    function(req, res, next) {
-    	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-        console.log('Request Type: ', fullUrl);
-        next();
+        function(req, res, next) {
+            var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+            console.log('Request URL: ', fullUrl);
+            next();
+        })
+    .use('/', require('./source/joelvaneenwyk/server'))
+    .use('/', require('./source/cowrk/server'))
+    .use(function(req, res) {
+        res.status(400);
+        res.render('404.ejs', { title: '404: File Not Found' });
+    })
+    .use(function(error, req, res, next) {
+        res.status(500);
+        res.render('500.ejs', { title: '500: Internal Server Error', error: error });
     });
-
-app
-.use('/', require('./source/joelvaneenwyk/server'))
-.use('/', require('./source/cowrk/server'))
-
-var expressBeautify = require('express-beautify')(/*Options*/);
-app.use(expressBeautify);
 
 port = process.env.PORT || 5000;
 
