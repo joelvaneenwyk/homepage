33a28a7b2a0bd550190b13e1470b87aaf89b1230
unknown <jovaneen@MININT-7K0E1D4.northamerica.corp.microsoft.com>
2016-06-18 23:07:14 -0700

    Front page now takes more advantage of Bootstrap to avoid the flicker at the beginning and now has an animation that blends the height change instead of snapping it.

--- source/joelvaneenwyk/data/js/main.js
+++ source/joelvaneenwyk/data/js/main.js
@@ -10,7 +10,7 @@ var loginUrl = '';
 var loginWindow = '';
 
 function startLogin() {
-    loginWindow = window.open(loginUrl, "Google Login", 'width=800, height=600'); 
+    loginWindow = window.open(loginUrl, "Google Login", 'width=800, height=600');
 }
 
 //
@@ -18,23 +18,17 @@ function startLogin() {
 //
 
 function updateHeader() {
-    var height = this.innerHeight / 2;
-    var width = this.innerWidth;
+    var height = Math.max( $(window).height() / 2, $("#bottom-align").height() );
+    var timeToDestination = 250;
+    $("#top-header").clearQueue();
+    $("#top-header").animate({ "height": height + 'px' }, {
+        duration: timeToDestination,
+        easing: "swing" });
 
-    //if (height < 250) height = 250;
-    if (width < 100) width = 100;
-
-    // Center the top header row by setting the width and the margin-left offset
-    var middleWidth = this.innerWidth;
-    if (middleWidth > 550) middleWidth = 550;
-
-    // Force the width of the main container to be the width of the window
-    $(".container.parent").css('width', width + 'px');
-    // Set the top row to be half the page height
-    $("div.row.top").css('height', height + 'px');
-
-    $(".row.header").css('width', middleWidth + 'px');
-    $(".row.header.centered").css('margin-left', '-' + (middleWidth / 2) + 'px');
+    $("#bottom-align").clearQueue();
+    $("#bottom-align").animate({ "margin-top": height - $("#bottom-align").height() + 'px' }, {
+        duration: timeToDestination,
+        easing: "swing" });
 }
 
 $(document).ready(function() {
@@ -48,7 +42,7 @@ $(document).ready(function() {
             window.clearInterval(interval);
             window.location = '/';
         }
-     }), 500);
+    }), 500);
 
     updateHeader();
     jQuery(window).on('resize', updateHeader);
