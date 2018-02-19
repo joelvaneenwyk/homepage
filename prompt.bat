0927766cab518b318e9511c1d02a52415b3663e0
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2018-02-19 15:04:49 -0800

    Updated packages to include grunt-harp as it stopped working without it.

--- prompt.bat
+++ prompt.bat
@@ -8,8 +8,12 @@ echo ===========================================================================
 echo  Joel Van Eenwyk (Homepage)
 echo ==============================================================================
 echo.
-echo 1) Run 'npm install' which will install dependencies and run 'grunt'
-echo 2) Run 'heroku local' to start the server
+echo 1) Install NodeJS / npm (https://nodejs.org/en/)
+echo 2) Install Heroku CLI (https://devcenter.heroku.com/articles/heroku-cli)
+echo 3) Run 'npm install -g grunt'
+echo 4) Run 'npm update'
+echo 5) Run 'npm install'
+echo 6) Run 'heroku local' to start the server
 echo.
 echo ==============================================================================
 echo.
