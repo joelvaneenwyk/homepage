fd30467070905e1c7a7c516a198a632cd11c94bf
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-08-11 15:53:58 -0700

    Optimize CI build

--- src/utilities/setup.sh
+++ src/utilities/setup.sh
@@ -15,35 +15,40 @@ fi
 
 apt-get install -y wget build-essential gcc g++ make git curl
 
-# Install Golang
-if [ ! -f "/tmp/go1.16.7.linux-amd64.tar.gz" ]; then
-    wget https://dl.google.com/go/go1.16.7.linux-amd64.tar.gz --directory-prefix=/tmp/
-fi
+if [ ! -x "$(command -v go)" ]; then
+    # Install Golang
+    if [ ! -f "/tmp/go1.16.7.linux-amd64.tar.gz" ]; then
+        wget https://dl.google.com/go/go1.16.7.linux-amd64.tar.gz --directory-prefix=/tmp/
+    fi
 
-if [ ! -d "/usr/local/go" ]; then
-    sudo tar -xvf "/tmp/go1.16.7.linux-amd64.tar.gz" --directory "/tmp/"
-    sudo mv "/tmp/go" "/usr/local"
-fi
+    if [ ! -d "/usr/local/go" ]; then
+        sudo tar -xvf "/tmp/go1.16.7.linux-amd64.tar.gz" --directory "/tmp/"
+        sudo mv "/tmp/go" "/usr/local"
+    fi
 
-export GOROOT="/usr/local/go"
-export PATH=$PATH:/usr/local/go/bin
+    export GOROOT="/usr/local/go"
+    export PATH=$PATH:/usr/local/go/bin
+    go env -w GOBIN=/usr/local/go/bin
+fi
 go version
-go env -w GOBIN=/usr/local/go/bin
 
 # Install NodeJS
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+if [ ! -x "$(command -v node)" ] || [ ! -x "$(command -v npm)" ]; then
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    sudo apt-get install -y nodejs
+fi
 
 # Install Yarn
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+if [ ! -x "$(command -v yarn)" ]; then
+    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+    echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt-get update && sudo apt-get install yarn
+fi
 
 # Install Hugo
-git clone -b "v0.87.0" "https://github.com/gohugoio/hugo.git" "/tmp/hugo"
-cd "/tmp/hugo" || true
-go install --tags extended
+if [ ! -x "$(command -v yarn)" ]; then
+    git clone -b "v0.87.0" "https://github.com/gohugoio/hugo.git" "/tmp/hugo"
+    cd "/tmp/hugo" || true
+    go install --tags extended
+fi
 hugo version
-
-# Install TypeScript and 'tsc'
-yarn global add typescript
