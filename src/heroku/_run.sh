839d52f847c8a982dc14d3591eb582ae744b927b
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-08-18 11:41:52 -0700

    chore: Cleanup and old file removal

--- src/heroku/_run.sh
+++ src/heroku/_run.sh
@@ -2,9 +2,9 @@
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && cd ../../ && pwd)"
 
-rm -rf "$ROOT_DIR/dist" >/dev/null 2>&1 || true
-rm -rf "$ROOT_DIR/resources" >/dev/null 2>&1 || true
-rm -rf "$ROOT_DIR/.yarn/cache" >/dev/null 2>&1 || true
+rm -rf "$ROOT_DIR/dist" > /dev/null 2>&1 || true
+rm -rf "$ROOT_DIR/resources" > /dev/null 2>&1 || true
+rm -rf "$ROOT_DIR/.yarn/cache" > /dev/null 2>&1 || true
 
 # shellcheck disable=SC1091
 . "$ROOT_DIR/src/utilities/setup.sh"
--- src/heroku/cleanup.sh
+++ src/heroku/cleanup.sh
@@ -2,5 +2,5 @@
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && cd ../../ && pwd)"
 
-rm -rf "$ROOT_DIR/.git" >/dev/null 2>&1 || true
-rm -rf "$ROOT_DIR/.yarn/cache" >/dev/null 2>&1 || true
+rm -rf "$ROOT_DIR/.git" > /dev/null 2>&1 || true
+rm -rf "$ROOT_DIR/.yarn/cache" > /dev/null 2>&1 || true
