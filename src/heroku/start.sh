b22309737ec95d87d5f5425e8bd6ead0b2b3dfdf
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-08-18 19:29:05 -0700

    Setup environment in setup command.

--- src/heroku/start.sh
+++ src/heroku/start.sh
@@ -5,6 +5,10 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && cd ../../ && pwd
 if [ ! -f "$ROOT_DIR/dist/server/server.js" ]; then
     # shellcheck disable=SC1091
     . "$ROOT_DIR/src/utilities/setup.sh"
+else
+    # Always setup the environment so that we can find 'golang' and 'hugo'
+    # shellcheck disable=SC1091
+    . "$ROOT_DIR/src/utilities/env.sh"
 fi
 
 yarn run --cwd "$ROOT_DIR" start
