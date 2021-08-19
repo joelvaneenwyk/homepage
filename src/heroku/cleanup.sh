6569973f20ca8eaf1602becc68d93d04e6d8c24b
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-08-18 17:34:29 -0700

    Remove post-build so that build runs

--- src/heroku/cleanup.sh
+++ src/heroku/cleanup.sh
@@ -4,3 +4,13 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && cd ../../ && pwd
 
 rm -rf "$ROOT_DIR/.git" > /dev/null 2>&1 || true
 rm -rf "$ROOT_DIR/.yarn/cache" > /dev/null 2>&1 || true
+
+if [ ! -f "$ROOT_DIR/dist/index.html" ]; then
+    echo "ERROR: Hugo site generation failed. Missing 'index.html' starting page."
+    exit 1
+fi
+
+if [ ! -f "$ROOT_DIR/dist/server/server.js" ]; then
+    echo "ERROR: Production server missing: '$ROOT_DIR/dist/server/server.js'"
+    exit 2
+fi
