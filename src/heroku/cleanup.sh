523a41a97c6cde157228e3ce4b1040c3ec872c67
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2021-08-18 22:38:04 -0700

    Re-clone git repository before build

--- src/heroku/cleanup.sh
+++ src/heroku/cleanup.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && cd ../../ && pwd
 rm -rf "$ROOT_DIR/.git" > /dev/null 2>&1 || true
 rm -rf "$ROOT_DIR/.yarn/cache" > /dev/null 2>&1 || true
 
-if [ ! -f "$ROOT_DIR/dist/index.html" ]; then
+if [ ! -f "$ROOT_DIR/dist/www/index.html" ]; then
     echo "ERROR: Hugo site generation failed. Missing 'index.html' starting page."
     exit 1
 fi
--- src/heroku/postbuild.sh
+++ src/heroku/postbuild.sh
@@ -6,4 +6,12 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && cd ../../ && pwd
 # shellcheck disable=SC1091
 . "$ROOT_DIR/src/utilities/env.sh"
 
+if [ ! -d "$ROOT_DIR/.git" ]; then
+    git clone --no-checkout "https://github.com/joelvaneenwyk/homepage.git" "$ROOT_DIR/dist/git"
+
+    # Move the .git folder to the directory with the files.
+    # This makes `existing-dir` a git repo.
+    mv "$ROOT_DIR/dist/git/.git" "$ROOT_DIR/"
+fi
+
 yarn run --cwd "$ROOT_DIR" build
