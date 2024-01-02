a8458f8288be11829a77fa0f464bcf403ee61f5b
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-02 09:39:14 -0800

    Re-enable eslint config settings
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -8,79 +8,54 @@ import js from "@eslint/js";
 const config = {
     files: ["**/src/safe/*.js"],
     env: {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "jquery": true,
-        "node": true
+        browser: true,
+        commonjs: true,
+        es6: true,
+        jquery: true,
+        node: true
     },
     //"extends": [
     //    "airbnb-base"
     //],
-    //"globals": {
-    //    "Atomics": "readonly",
-    //    "SharedArrayBuffer": "readonly"
-    //},
-    //"parser": "@typescript-eslint/parser",
-    //"parserOptions": {
-    //    "ecmaVersion": 2018
-    //},
-    //"plugins": [
-    //    "@typescript-eslint"
-    //],
-    //"ignorePatterns": [
-    //    "themes/base/assets/lib/**",
-    //    "src/server/cheeses/output/**",
-    //    "dist/**",
-    //    "**/*.min.js",
-    //    "**/newrelic.js"
-    //],
-    //"rules": {
-    //    "indent": [
-    //        "error",
-    //        4
-    //    ],
-    //    "max-len": [
-    //        "error",
-    //        {
-    //            "code": 130
-    //        }
-    //    ],
-    //    "linebreak-style": "off",
-    //    "quotes": [
-    //        "error",
-    //        "double"
-    //    ],
-    //    "semi": [
-    //        "error",
-    //        "always"
-    //    ],
-    //    "arrow-body-style": [
-    //        "error",
-    //        "as-needed"
-    //    ],
-    //    "prefer-arrow-callback": "off",
-    //    "sort-imports": "off",
-    //    "no-cond-assign": [
-    //        "error",
-    //        "always"
-    //    ],
-    //    "import/first": "off",
-    //    "comma-dangle": [
-    //        "error",
-    //        {
-    //            "arrays": "never",
-    //            "objects": "never",
-    //            "imports": "never",
-    //            "exports": "never",
-    //            "functions": "never"
-    //        }
-    //    ],
-    //    "no-console": "off"
-    //},
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    plugins: ["@typescript-eslint"],
+    ignorePatterns: ["themes/base/assets/lib/**", "dist/**", "**/*.min.js", "**/newrelic.js"],
+    rules: {
+        indent: ["error", 4],
+        "max-len": [
+            "error",
+            {
+                code: 130
+            }
+        ],
+        "linebreak-style": "off",
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "arrow-body-style": ["error", "as-needed"],
+        "prefer-arrow-callback": "off",
+        "sort-imports": "off",
+        "no-cond-assign": ["error", "always"],
+        "import/first": "off",
+        "comma-dangle": [
+            "error",
+            {
+                arrays: "never",
+                objects: "never",
+                imports: "never",
+                exports: "never",
+                functions: "never"
+            }
+        ],
+        "no-console": "off"
+    },
     ...js.configs.recommended
 };
 
-export default [
-    config
-];
+export default [config];
