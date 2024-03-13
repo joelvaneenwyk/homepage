121a124c67b9d200c971b4cd5d3407bf484cb836
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-12 17:02:31 -0700

    Updated setup script

--- setup.bat
+++ setup.bat
@@ -1,7 +1,12 @@
-@echo off
+@echo off && goto:$Main
 
+:$Main
 setlocal EnableDelayedExpansion
+    call yarn config set --home enableTelemetry 0
+    call yarn install
 
-call yarn config set --home enableTelemetry 0
-call yarn install
-call yarn build
+    set "CGO_ENABLED=1"
+    call proto run go -- run -tags extended github.com/gohugoio/hugo@v0.123.8
+
+    :: call yarn build
+endlocal & exit /b %errorlevel%
